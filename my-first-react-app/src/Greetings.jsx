d4975afd328fe274f3eff45c21d6291f37124095
Yordanos Yeshitla <yordiman614@gmail.com>
2024-10-29 21:50:53 +0300

    i have added new jsx files to my react app and practiced the creation of components and how i can return them.

---
+++ my-first-react-app/src/Greetings.jsx
@@ -0,0 +1,9 @@
+function Greeting(){
+    return <h1>&quot;Hey buddy how you doing?&quot;</h1>;
+}
+
+export const Food = ()=>{
+    return <h2>My favorite food is banana.</h2>
+}
+
+export default Greeting;
