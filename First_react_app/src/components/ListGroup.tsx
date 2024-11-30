1344b74a05508abbf9a0ce7a8ccab99430ac8c64
Yordanos Yeshitla <yordiman614@gmail.com>
2024-11-30 21:52:39 +0300

    .

---
+++ First_react_app/src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+  const items = ["New York", "San Franciscon", "Tokyo", "London"];
+
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
