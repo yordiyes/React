80506a482a7128124fa0effd3774740f29f85298
Yordanos Yeshitla <yordiman614@gmail.com>
2024-12-03 20:14:27 +0300

    finished the final exercise on styled components

--- First_react_app/src/App.tsx
+++ First_react_app/src/App.tsx
@@ -1,20 +1,9 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/List Group/ListGroup";
+import Like from './components/Like'
 
 function App() {
-  const items = ["Addis", "Dire", "Harar"]
-  const [alertVisible, setAlertVisible] = useState(false);
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisible(false)}>Alert</Alert>
-      )}
-      <Button color="primary" onClick={() => setAlertVisible(true)}>
-        Primary
-      </Button>
-      <ListGroup items={items} heading="Cities in Ethipia" onSelectItem={()=>console.log("hello")}/>
+      <Like onClick={()=>console.log("Clicked")} />
     </div>
   );
 }
