a16e1151ed5abedcc2afb4ef44695d5922b54039
Yordanos Yeshitla <yordiman614@gmail.com>
2024-12-04 21:08:43 +0300

    exercise 3 done

--- First_react_app/src/App.tsx
+++ First_react_app/src/App.tsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"]
+  const [cart, setCart] = useState({
+    discount: .1,
+    items: [
+      {id: 1, title: "Product 1", quantity: 1},
+      {id: 2, title: "Product 2", quantity: 1}
+    ]
   })
 
   const handleClick = () =>{
     // change the name of the player in to something else 
-    setPizza({...pizza, toppings: [...pizza.toppings, "Cheese"]})
+    setCart({...cart, items: cart.items.map(item => {item.id == 1? {...item, quantity: item.quantity + 1}: item})})
   }
   return (
     <div>
