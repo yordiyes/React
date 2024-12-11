08356bb8125e1a2bb48a1392b85bb5337d7d45ec
Yordanos Yeshitla <yordiman614@gmail.com>
2024-12-11 08:37:45 +0300

    started working on building form module

--- First_react_app/src/App.tsx
+++ First_react_app/src/App.tsx
@@ -1,10 +1,8 @@
-import ExpandableText from './components/ExpandableText';
+import Form from './components/Form';
 
 function App() {
   return (
-    <ExpandableText>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam officia error optio eveniet distinctio, consectetur illo eaque modi, beatae ex amet explicabo asperiores sequi quas, dolor ratione ipsam! Non modi soluta, repellat facilis tempore vero repudiandae nulla omnis excepturi tenetur architecto veritatis ad laborum veniam delectus voluptatibus? Eaque, sint possimus, dolor nemo illo enim magnam quia amet sed molestias, quae corrupti ullam aut. Dolorem, necessitatibus doloribus sapiente earum accusamus dolor soluta pariatur est autem facere ab laborum cupiditate. Repellat iure quam sequi eligendi ipsum voluptas ipsam nemo fugiat, tenetur sint impedit mollitia error, praesentium fuga laboriosam unde laborum, neque et.
-    </ExpandableText>
+    <Form/>
   );
 }
 
