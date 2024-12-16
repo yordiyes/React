13bcab34ab8de68151cd86cc9c79f6c07df80539
Yordanos Yeshitla <yordiman614@gmail.com>
2024-12-16 11:13:29 +0300

    http requests

--- First_react_app/src/App.tsx
+++ First_react_app/src/App.tsx
@@ -9,30 +9,47 @@ interface User {
 export default function ProductList() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await axios.get<User[]>(
-          "https://jsonplaceholder.typicode.com/usersx"
-        );
+    //we can fetch datat using axios by two methods, one is using await async method which is like:-
+
+    // const fetchUser = async () => {
+    //   try {
+    //     const res = await axios.get<User[]>(
+    //       "https://jsonplaceholder.typicode.com/users"
+    //     );
+    //     console.log(res);
+    //     setUsers(res.data);
+    //   } catch (err) {
+    //     setError((err as AxiosError).message);
+    //     console.log(err)
+    //   }
+    // };
+    // fetchUser();
+
+    // the  second method is using .then like:-
+    setIsLoading(true)
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
         setUsers(res.data);
-      } catch (err) {
-        setError((err as AxiosError).message);
-      }
-
-      fetchUser();
-    };
-
-    // .then((res) => setUsers(res.data))
-    // .catch(err => setError(err.message))
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
   }, []);
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
-      <ul>
+      {isLoading && <div className="spinner-border"></div>}
+      <ul className="list-group list-group-flush">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id} className="list-group-item">
+            {user.name}
+          </li>
         ))}
       </ul>
     </>
