eb755e8cf6b3d6fc3bf4deb00c39c6796fed7fc1
Yordanos Yeshitla <yordiman614@gmail.com>
2024-12-16 11:26:26 +0300

    delete requet

--- First_react_app/src/App.tsx
+++ First_react_app/src/App.tsx
@@ -11,6 +11,17 @@ export default function ProductList() {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
+  const deleteUser = (user: User) => {
+    const originalusers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalusers); 
+      });
+  };
+
   useEffect(() => {
     //we can fetch datat using axios by two methods, one is using await async method which is like:-
 
@@ -29,7 +40,7 @@ export default function ProductList() {
     // fetchUser();
 
     // the  second method is using .then like:-
-    setIsLoading(true)
+    setIsLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
@@ -47,8 +58,18 @@ export default function ProductList() {
       {isLoading && <div className="spinner-border"></div>}
       <ul className="list-group list-group-flush">
         {users.map((user) => (
-          <li key={user.id} className="list-group-item">
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
             {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+            {/* <button className="btn btn-outline-danger">Add</button> */}
           </li>
         ))}
       </ul>
